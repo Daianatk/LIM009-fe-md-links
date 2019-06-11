c7be4b2de6183172adf10ba2e478997ca1c9f813
Daianatk <daiana.campos04@gmail.com>
2019-06-11 17:36:15 -0500

    Absoluta y Relativa

--- cli.js
+++ cli.js
@@ -1,2 +1,15 @@
+#!/usr/bin/env node
 const[,, ...args]= process.argv
 console.log(`md links ${args}`);
+
+let path = require ('path');
+
+const ruta= `${args}`;
+const abs= (ruta) =>{
+    if(path.isAbsolute(ruta) === true){
+        return console.log('Es una ruta absoluta');
+    }else{
+        console.log('Es una ruta Relativa');
+    }
+}
+abs(ruta)
